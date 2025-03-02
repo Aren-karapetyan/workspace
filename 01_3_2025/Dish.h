7ea1089190049b29aae2b6d08fbc42da5b1df8c9
Aren-karapetyan <karapetyan.aren2006@gmail.com>
2025-03-02 15:34:24 +0400

    Թարմացրեցի ֆայլերը, ավելացրեցի նոր ֆայլեր ու ջնջեցի հիները

--- 01_3_2025/Dish.h
+++ 01_3_2025/Dish.h
@@ -9,29 +9,6 @@ public:
 	Dish(Dish&& other) noexcept;
 	Dish& operator=(Dish&& other) noexcept;
 	Dish::Dish(std::string dishName, double dishPrice)
-    : name(std::move(dishName)), price(dishPrice) {}
-
-Dish::Dish(const Dish& other)
-    : name(other.name), price(other.price) {}
-
-Dish& Dish::operator=(const Dish& other) {
-    if (this != &other) {
-        name = other.name;
-        price = other.price;
-    }
-    return *this;
-}
-
-Dish::Dish(Dish&& other) noexcept
-    : name(std::move(other.name)), price(other.price) {}
-
-Dish& Dish::operator=(Dish&& other) noexcept {
-    if (this != &other) {
-        name = std::move(other.name);
-        price = other.price;
-    }
-    return *this;
-}
 
 
 	void display() const;
